2316ce90d48a9014373bacf17ca78a1d4552628f
Ilham Meidi Lenovo <ilham.meidi@gmail.com>
2026-01-27 23:47:54 +0700

    next app router

---
+++ app/components/Footer/Footer.js
@@ -0,0 +1,71 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import Container from '@mui/material/Container';
+import Grid from '@mui/material/Grid';
+import Box from '@mui/material/Box';
+import { useTheme } from '@mui/material/styles';
+import useStyles from './footer-style';
+
+function Copyright() {
+  return (
+    <Typography variant="body2" display="block" color="textSecondary">
+      &copy;&nbsp;
+      {brand.starter.footerText}
+    </Typography>
+  );
+}
+
+const footers = [
+  {
+    title: 'Company',
+    description: ['Team', 'History', 'Contact us', 'Locations'],
+    link: ['#team', '#history', '#contact-us', '#locations'],
+  },
+  {
+    title: 'Resources',
+    description: ['Resource', 'Resource name', 'Another resource', 'Final resource'],
+    link: ['#resource', '#resource-name', '#another-resource', '#final-resource'],
+  },
+  {
+    title: 'Legal',
+    description: ['Privacy policy', 'Terms of use'],
+    link: ['#privacy-policy', '#terms-of-use'],
+  },
+];
+
+function Footer(props) {
+  const { toggleDir } = props;
+  // Theme breakpoints
+  const theme = useTheme();
+  const isDesktop = useMediaQuery(theme.breakpoints.up('md'));
+  const isMobile = useMediaQuery(theme.breakpoints.down('md'));
+
+  // Translation Function
+  const { t } = useTranslation('common');
+
+  const { classes } = useStyles();
+  const { classes: align } = useTextAlign();
+
+  return (
+    <Container maxWidth="lg" component="footer" className={classes.footer}>
+      <h2>Hello Footer</h2>
+      {isMobile && (
+        <div className={align.textCenter}>
+          <Box p={4}>
+            <Copyright />
+          </Box>
+        </div>
+      )}
+    </Container>
+  );
+}
+
+Footer.propTypes = {
+  toggleDir: PropTypes.func,
+};
+
+Footer.defaultProps = {
+  toggleDir: () => {},
+};
+
+export default Footer;
